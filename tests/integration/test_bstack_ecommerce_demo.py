30dde067c1e894b334f00640d1f0d7937339ccb5
German Chaverri Campos <germanch90@gmail.com>
2023-12-24 16:32:53 -0600

    Add BrowserStack demo ecommerce page and test cases

---
+++ tests/integration/test_bstack_ecommerce_demo.py
@@ -0,0 +1,51 @@
+import pytest
+from playwright.sync_api import expect
+from tests.pages.browserstack_demo_page import BrowserStackDemoPage, SmartphoneBrand
+
+# Firstly, let's create a fixture that will be used to start each test on the BrowserStack demo ecommerce page.
+@pytest.fixture(scope="function")
+def browserstack_demo_page(page):
+    """Start each test on the BrowserStack demo ecommerce page."""
+    browserstack_demo_page = BrowserStackDemoPage(page)
+    browserstack_demo_page.navigate()
+    return browserstack_demo_page
+
+@pytest.fixture(scope="module")
+def smartphone_brand():
+    """Fixture to get a smartphone brand."""
+    return SmartphoneBrand
+
+# Now, let's create a test class for the BrowserStack demo ecommerce page.
+class TestBstackDemo:
+
+    def test_navigation(self, browserstack_demo_page):
+        """Test that the BrowserStack demo ecommerce page can be navigated to."""
+        # Expect the page to have the correct title.
+        expect(browserstack_demo_page.page).to_have_title("StackDemo")
+
+    def test_count_products(self, browserstack_demo_page):
+        """Test that the BrowserStack demo ecommerce page has the correct number of products."""
+        # Expect the page to have 25 products.
+        expect(browserstack_demo_page.product_count).to_have_text("25 Product(s) found.")
+
+        expect(browserstack_demo_page.products).to_have_count(25)
+
+    @pytest.mark.parametrize(
+        "brand, count",
+        (
+            (SmartphoneBrand.APPLE, 9),
+            (SmartphoneBrand.SAMSUNG, 7),
+            (SmartphoneBrand.GOOGLE, 3),
+            (SmartphoneBrand.ONEPLUS, 6),
+        )
+    )
+    def test_filter_by_brand(self, browserstack_demo_page, brand, count):
+        """Test that the BrowserStack demo ecommerce page can be filtered by brand."""
+        # Expect the page to have 25 products initially.
+        expect(browserstack_demo_page.product_count).to_have_text("25 Product(s) found.")
+
+        # Filter by brand "Samsung".
+        browserstack_demo_page.filter_by_brand(brand)
+
+        # Expect the page to have 5 products.
+        expect(browserstack_demo_page.product_count).to_have_text(f"{count} Product(s) found.")
---
+++ tests/pages/browserstack_demo_page.py
@@ -0,0 +1,39 @@
+from playwright.sync_api import Page
+from enum import Enum
+
+class SmartphoneBrand(Enum):
+    APPLE = "Apple"
+    SAMSUNG = "Samsung"
+    GOOGLE = "Google"
+    ONEPLUS = "OnePlus"
+
+class BrowserStackDemoPage():
+    """
+    Page model for the BrowserSatck demo ecommerce page.
+    
+    The page is composed by a grid of products that can be added to a cart, filtered by brand and sorted.
+    """
+
+    def __init__(self, page: Page) -> None:
+        """Constructor for the BrowserStackDemoPage class."""
+        self.page = page
+        self.product_count = self.page.get_by_text("Product(s) found")
+
+    def navigate(self):
+        """Navigate to the BrowserStack demo ecommerce page."""
+        self.page.goto("https://bstackdemo.com/")
+
+    @property
+    def products(self):
+        """Get all products."""
+        return self.page.locator("div.shelf-item")
+    
+    def filter_by_brand(self, brand: SmartphoneBrand):
+        """
+        Filter products by brand.
+
+        Args:
+            brand (SmartphoneBrand): The brand to filter by.
+        """
+        self.page.locator("span.checkmark").filter(has_text=brand.value).click()
+
